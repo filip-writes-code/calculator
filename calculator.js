ba9a0bed8b625b08ce26ae0f94082b9a51471c8a
Filip <tjofee47@gmail.com>
2025-03-10 16:58:57 -0600

    Add functions to the calculator, and some basic UI

--- calculator.js
+++ calculator.js
@@ -1,22 +1,24 @@
-function add (a, b) {
-    return a + b;
-};
+const display = document.querySelector('.display');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('#clear');
+const reverseButton = document.querySelector('#reverse');
+const percentageButton = document.querySelector('#percentage');
 
-function subtract (a, b) {
-    return a - b;
-};
 
-function multiply(a, b) {
-    return a * b;
-};
 
-function divide(a, b) {
-    return a / b;
-};
+let firstValue = '';
+let secondValue = '';
+let operatorValue = '';
 
-function operate(numberOne, numberTwo, operator) {
+function add (a, b) {return a + b;};
+function subtract (a, b) {return a - b;};
+function multiply(a, b) {return a * b;};
+function divide(a, b) {return a / b;};
+
+function operate() {
     let operation;
-    switch (operator) {
+    switch (operatorValue) {
         case "+":
             operation = add;
             break;
@@ -30,5 +32,64 @@ function operate(numberOne, numberTwo, operator) {
             operation = divide;
             break;
         };
-    return operation(numberOne, numberTwo);
+    return operation(+firstValue, +secondValue).toString();
 };
+
+function clear() {
+        firstValue = '';
+        secondValue = '';
+        operatorValue = '';
+        refreshDisplay(0);
+};
+
+function refreshDisplay(input) {
+    display.innerText = input;
+};
+
+function numberClicked(number) {
+    if (!operatorValue) {
+        firstValue += number;
+        refreshDisplay(firstValue);
+    } else {
+        secondValue += number;
+        refreshDisplay(secondValue);
+    };
+};
+
+function operatorClicked(operation) {
+    if (firstValue && operatorValue && secondValue) 
+        {
+        const result = operate();
+        refreshDisplay(result);
+        firstValue = result;
+        secondValue = '';
+        }
+
+    operatorValue = operation;
+};
+
+function positiveNegativeSwitch() {
+    if (!operatorValue) {
+        firstValue = (+firstValue * -1).toString();
+        refreshDisplay(firstValue);
+    } else {
+        secondValue = (+secondValue * -1).toString();
+        refreshDisplay(secondValue);
+    };
+};
+
+function makePercentage() {
+    if (!operatorValue) {
+        firstValue = (+firstValue / 100).toString();
+        refreshDisplay(firstValue);
+    } else {
+        secondValue = (+secondValue / 100).toString();
+        refreshDisplay(secondValue);
+    };
+}
+
+numberButtons.forEach(button => button.addEventListener('click', () => numberClicked(button.id)));
+operatorButtons.forEach(button => button.addEventListener('click', () => operatorClicked(button.id)));
+clearButton.addEventListener('click', () => clear());
+reverseButton.addEventListener('click', () => positiveNegativeSwitch());
+percentageButton.addEventListener('click', () => makePercentage());
